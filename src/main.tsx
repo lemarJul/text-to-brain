be6269a1b2b6c9e1de9d6746a21c5e285c25a4a0
Julien Lemarchand <lemar.jul@gmail.com>
2024-06-16 00:08:38 +0200

    feat: integrate AppContext provider into main application

--- src/main.tsx
+++ src/main.tsx
@@ -10,12 +10,15 @@ import { ThemeProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
 import theme from "./theme";
 import App from "./App";
+import { AppProvider } from "./utils/context";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
+    {/* <ThemeProvider theme={theme}> */}
+    {/* <CssBaseline /> */}
+    <AppProvider>
       <App />
-    </ThemeProvider>
+    </AppProvider>
+    {/* </ThemeProvider> */}
   </React.StrictMode>
 );
