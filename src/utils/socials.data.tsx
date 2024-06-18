da28fe5383eca4781e7486baa1bc58f4f17417e4
Julien Lemarchand <lemar.jul@gmail.com>
2024-06-18 16:44:29 +0200

    feat(utils): add social links data for GitHub, LinkedIn, Instagram

---
+++ src/utils/socials.data.tsx
@@ -0,0 +1,21 @@
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+
+export default [
+  {
+    // GitHub
+    href: "https://github.com/lemarJul",
+    icon: <GitHubIcon />,
+  },
+  {
+    // LinkedIn
+    href: "https://www.linkedin.com/in/julien-lemarchand/",
+    icon: <LinkedInIcon />,
+  },
+  {
+    // Instagram
+    href: "https://www.instagram.com/lemarjul/",
+    icon: <InstagramIcon />,
+  },
+];
