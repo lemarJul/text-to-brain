88e0d0dbe58c7c5567582cb8141dd2b13d2ba5ca
Julien Lemarchand <lemar.jul@gmail.com>
2024-06-18 17:08:31 +0200

    feat(components): add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+export default function Header() {
+  return (
+    <header>
+      <Typography variant="h1" component="h1">
+        Text to Brain
+      </Typography>
+    </header>
+  );
+}
