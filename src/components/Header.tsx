e1c2178f0f386047a78d2eacf7069da9b8cece24
Julien Lemarchand <lemar.jul@gmail.com>
2024-06-19 21:39:30 +0200

    feat(header): integrate InfoButton with about toggle in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,25 @@
-import { Typography } from "@mui/material";
+import { Typography, Box } from "@mui/material";
+import InfoButton from "./buttons/Button.info";
+import { useAppContext } from "../utils/context";
 
 export default function Header() {
+  const { setShowAbout } = useAppContext();
+  const ToggleAbout = () => setShowAbout((prev) => !prev);
   return (
-    <header>
-      <Typography variant="h1" component="h1">
+    <Box
+      component="header"
+      sx={{
+        position: "relative",
+        button: {
+          position: "absolute",
+          top: "1rem",
+        },
+      }}
+    >
+      <Typography variant="h1" component="h1" display={"inline"}>
         Text to Brain
       </Typography>
-    </header>
+      <InfoButton onClick={ToggleAbout} />
+    </Box>
   );
 }
