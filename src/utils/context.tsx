ac464b476f5d95fa0395418739da04aa062ebfa1
Julien Lemarchand <lemar.jul@gmail.com>
2024-06-19 21:33:38 +0200

    perf(context): memoize AppContext.Provider value with useMemo

--- src/utils/context.tsx
+++ src/utils/context.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useMemo } from "react";
 
 type AppContextType = {
   difficulty: 0 | 1 | 2 | 3;
@@ -9,6 +9,8 @@ type AppContextType = {
   setShowWordLength: React.Dispatch<React.SetStateAction<boolean>>;
   textContent: string;
   setTextContent: (text: string) => void;
+  showAbout: boolean;
+  setShowAbout: React.Dispatch<React.SetStateAction<boolean>>;
   // Add other state variables here
 };
 
@@ -23,27 +25,44 @@ export const AppProvider: React.FC<{ children: React.ReactNode }> = ({
   const [textContent, setTextContent] = useState(
     localStorage.getItem("textContent") || ""
   );
+  const [showAbout, setShowAbout] = useState(false);
   // Initialize other state variables here
 
-  return (
-    <AppContext.Provider
-      value={{
-        difficulty,
-        setDifficulty,
-        editMode,
-        setEditMode /* Add other state variables here */,
-        showWordLength,
-        setShowWordLength,
-        textContent,
-        setTextContent: (text: string) => {
-          //save to local storage
-          localStorage.setItem("textContent", text);
-          setTextContent(text);
-        },
-      }}
-    >
-      {children}
-    </AppContext.Provider>
+  return useMemo(
+    () => (
+      <AppContext.Provider
+        value={{
+          difficulty,
+          setDifficulty,
+          editMode,
+          setEditMode /* Add other state variables here */,
+          showWordLength,
+          setShowWordLength,
+          textContent,
+          setTextContent: (text: string) => {
+            //save to local storage
+            localStorage.setItem("textContent", text);
+            setTextContent(text);
+          },
+          showAbout,
+          setShowAbout,
+        }}
+      >
+        {children}
+      </AppContext.Provider>
+    ),
+    [
+      difficulty,
+      setDifficulty,
+      editMode,
+      setEditMode,
+      showWordLength,
+      setShowWordLength,
+      textContent,
+      setTextContent,
+      showAbout,
+      setShowAbout,
+    ]
   );
 };
 
