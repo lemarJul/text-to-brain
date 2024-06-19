2ef15f2da92fdd83ae8d1c1f2efbbcd791f57662
Julien Lemarchand <lemar.jul@gmail.com>
2024-06-19 22:28:32 +0200

    feat(App): toggle between About and Card components
    - Implement dynamic display of About and Card components using Collapse
    from MUI.
    - Utilize useAppContext for managing the visibility state of
    components.
    - Enhance UX with smooth transitions for component toggling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 //Material UI Components
-import { Container } from "@mui/material";
+import { Container, Collapse } from "@mui/material";
 // Custom Components
 import Header from "./components/Header";
 import Card from "./components/Card.textToBrain";
 import Footer from "./components/Footer";
+import About from "./components/About";
 // Styles
 import "./App.css";
+import { useAppContext } from "./utils/context";
 
 function App() {
+  const { showAbout } = useAppContext();
   return (
     <Container
       sx={{
@@ -17,8 +20,22 @@ function App() {
       }}
     >
       <Header />
+
       <main>
-        <Card />
+        <Collapse
+          in={showAbout}
+          timeout={500}
+          easing={{ enter: "easeOut", exit: "easeIn" }}
+        >
+          <About />
+        </Collapse>
+        <Collapse
+          in={!showAbout}
+          timeout={500}
+          easing={{ enter: "easeOut", exit: "easeIn" }}
+        >
+          <Card />
+        </Collapse>
       </main>
       <Footer />
     </Container>
